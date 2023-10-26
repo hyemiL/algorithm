1ffa2e1b892d2a4a3580f1fea9e9a5d292a63228
hyemiL <89233243+hyemiL@users.noreply.github.com>
2023-10-26 14:36:09 +0900

    [level unrated] Title: 두 수의 곱, Time: 0.01 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        boolean checkNumArange = false;
+        int answer = 0;
+
+        if((0 <= num1 && num1 <=100) && (0 <= num2 && num2 <= 100)) {
+            checkNumArange = true;
+        }
+
+        if(checkNumArange) { answer =  num1 * num2; }
+        return answer;
+    }
+}
