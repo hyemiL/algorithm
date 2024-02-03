4eef81e67e8b3cabefbd746ed19808ff55fe4898
hyemiL <89233243+hyemiL@users.noreply.github.com>
2024-02-03 23:37:04 +0900

    fix 배열의 평균값 타인 풀이 참고

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -7,4 +7,7 @@ class Solution {
         answer = answer / numbers.length;
         return answer;
     }
+
+    // 다른 사람의 풀이 참고한 스트림 코드,,, 스트림 공부의 필요성 느낌
+    // return Arrays.stream(numbers).average().orElse(0);
 }
