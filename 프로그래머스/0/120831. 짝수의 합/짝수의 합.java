6957a9b845619b3eb4855abb6e05132e6567ad85
hyemiL <89233243+hyemiL@users.noreply.github.com>
2024-02-03 23:31:06 +0900

    fix 짝수의 합 풀이 시 고려사항

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -8,4 +8,11 @@ class Solution {
         }
         return answer;
     }
+
+    // for문 안에서 if문으로 분기할 생각을 했는데 문제 자체가 짝수만 더하는 거니까 애초에 for문의 조건을 2부터 시작해서 +2씩 가면 굳이 다 돌 필요 없고 더 효율적이었겠다....!
+
+    //타인 정답에서 배운 람다식
+    //return IntStream.rangeClosed(0, n)
+    //.filter(e -> e % 2 == 0)
+    //.sum();
 }
