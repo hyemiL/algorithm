698a6624c465cf4681767da0579d9b36aae04083
hyemiL <89233243+hyemiL@users.noreply.github.com>
2024-02-04 00:11:44 +0900

    fix: 피자 나눠 먹기(1) 타인 풀이 참고 업데이트

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -3,4 +3,8 @@ class Solution {
         int answer = n % 7 == 0 ? n / 7 : n / 7 + 1;
         return answer;
     }
+
+    // 타인 풀이 참고
+    // return (n+6) / 7;
+    // 6은 어떤 수를 7로 나누면 나머지가 0부터 6까지만 존재.
 }
